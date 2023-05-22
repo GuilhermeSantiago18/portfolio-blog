0e5034d45f7d952c2d68995d056e3b75a438cd64
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2023-05-22 12:01:52 -0300

    chore: testing new functions of Nextjs

---
+++ frontend/src/app/home/loading.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function loading() {
+  return (
+ 
+    <p>loading</p>
+  )
+}
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,5 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { ReactNode } from "react"
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,11 +9,12 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: ReactNode
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <h1>BLOGSPOT</h1>
+      <body>{children}</body>
     </html>
   )
 }
