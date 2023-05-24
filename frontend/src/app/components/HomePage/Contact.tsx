d38b74bf5075563b42f31e823a1aa0860b7b8a41
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2023-05-24 17:59:04 -0300

    feat: contact componente

--- frontend/src/app/components/HomePage/Contact.tsx
+++ frontend/src/app/components/HomePage/Contact.tsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import React from 'react';
 
-export default function Contact() {
+const Contato: React.FC = () => {
   return (
-    <div id="contactSection" className='mb-96'>Contact</div>
-  )
-}
+    <section className="bg-th-secondary text-th-primary py-8 mt-10">
+      <div id="contactSection" className="container mx-auto px-4">
+        <h2 className="text-3xl font-bold mb-4">Entre em contato</h2>
+        <p className="text-lg mb-6">Agende sua consulta, entre em contato conosco:</p>
+        <div className="flex flex-wrap -mx-2">
+          <div className="w-full md:w-1/2 lg:w-1/4 px-2 mb-4">
+            <h3 className="text-xl font-semibold mb-2">Telefone</h3>
+            <p className="text-lg">+55 084 9 8799-0420</p>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/4 px-2 mb-4">
+            <h3 className="text-xl font-semibold mb-2">Email</h3>
+            <p className="text-lg">contato@example.com</p>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/4 px-2 mb-4">
+            <h3 className="text-xl font-semibold mb-2">Endereço</h3>
+            <p className="text-lg">123 Rua Principal</p>
+            <p className="text-lg">Natal, Rio Grande do Norte</p>
+            <p className="text-lg">CEP 12345-678</p>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/4 px-2 mb-4">
+            <h3 className="text-xl font-semibold mb-2">Redes Sociais</h3>
+            <div className="flex space-x-4">
+              <a
+                href="https://www.facebook.com/exemplo"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-lg"
+              >
+                Facebook
+              </a>
+              <a
+                href="https://www.instagram.com/exemplo"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-lg"
+              >
+                Instagram
+              </a>
+              <a
+                href="https://www.twitter.com/exemplo"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-lg"
+              >
+                Twitter
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contato;
--- frontend/src/components/WhattsButton.tsx
+++ frontend/src/components/WhattsButton.tsx
@@ -4,7 +4,9 @@ import React from 'react'
 
 export default function WhattsButton() {
     return (
-        <Link href="https://api.whatsapp.com/send?phone=55084987990420" className="fixed bottom-4 right-2 z-10 flex items-center justify-center w-10 h-10 p-1 bg-white rounded-full shadow-lg cursor-pointer hover:bg-green-400">
+        <Link href="https://api.whatsapp.com/send?phone=55084987990420" 
+        className="fixed bottom-4 right-2 z-10 flex items-center justify-center w-10 h-10 p-1
+         bg-white rounded-full shadow-lg cursor-pointer hover:bg-green-400">
             <Image src={'/whatsapp.svg'} width={30} height={30} alt="Whatsapp"/>
         </Link>
     )
