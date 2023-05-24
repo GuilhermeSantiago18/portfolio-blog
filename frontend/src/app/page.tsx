e794006545c5ff9174696e46b1ac9846b1050bd6
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2023-05-24 13:41:33 -0300

    feat:depoiments area

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import AboutMe from "./components/HomePage/AboutMe";
+import Depoiments from "./components/HomePage/Depoiments";
 
 export default function Home() {
   return (
     <div className='container'>
     <AboutMe />
+    <Depoiments />
     </div>
   )
 }
