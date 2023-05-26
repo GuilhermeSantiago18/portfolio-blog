13540f4d54fba2754a23558671f79a7db14b45da
julio-silveira <julioqueiroz15@gmail.com>
2023-05-26 17:59:04 -0300

    feat: update

--- frontend/src/app/components/HomePage/AboutMe.tsx
+++ frontend/src/app/components/HomePage/AboutMe.tsx
@@ -1,19 +1,18 @@
 import Image from 'next/image';
 import React from 'react';
 
-
 const AboutMe: React.FC = () => {
   return (
-    <div className="flex flex-wrap items-start justify-center mr-10" id="aboutSection">
-      <div className="md:w-1/2 lg:w-1/2 xl:w-1/2 mb-4">
+    <div className="flex flex-col-reverse lg:flex-row justify-start lg:justify-center items-center mr-10 h-screen" id="aboutSection">
+      <div className="w-full lg:w-1/2 bg-th-secondary rounded-full lg:rounded-none flex justify-center lg:bg-th-background overflow-hidden mt-2 lg:mt-0">
         <Image
-          src="/potiraAbout.jpg"
+          src="/medic.png"
           alt="Dra. Potira"
-          width={1000}
-          height={1000}
+          width={200}
+          height={200}
         />
       </div>
-      <div className="md:w-1/2 lg:w-1/2 xl:w-1/2 mt-32 px-4">
+      <div className="w-full lg:w-1/2 xl:w-1/2 px-4 text-center lg:text-left mt-0 lg:mt-32 lg:self-start">
         <h2 className="text-2xl text-th-secondary font-bold mb-4">Dra. Potira</h2>
         <p className='text-th-secondary'>
           Dra. Potira é uma renomada endocrinologista formada na UFRN. Ela possui vasta experiência e conhecimento na área de endocrinologia, com ênfase em tratamento de distúrbios hormonais e doenças da tireoide.
--- frontend/src/app/components/HomePage/HomePage.tsx
+++ frontend/src/app/components/HomePage/HomePage.tsx
@@ -2,13 +2,20 @@ import React from 'react'
 import AboutMe from './AboutMe'
 import Depoiments from './Depoiments'
 import StaticDepoiments from './StaticDepoiments'
+import { GenericSection } from '@/app/blog/components'
+import { mockPosts } from '@/app/blog/mock/data'
 
 export default function HomePage() {
     return (
         <main>
             <AboutMe />
-            <StaticDepoiments />
-            <Depoiments />
+            <div className="h-screen">
+                <StaticDepoiments />
+                <Depoiments />
+            </div>
+            <div className='p-2'>
+            <GenericSection sectionName='Posts Recentes' posts={mockPosts.slice(0,3)} />
+            </div>
         </main>
     )
 }
